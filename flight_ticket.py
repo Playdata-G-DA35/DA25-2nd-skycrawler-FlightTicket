b2f08af82246cc79814ed420b3246236dd42780d
ga0keem <kky965912@gmail.com>
2024-04-01 17:54:45 +0900

    ticket crawling

--- flight_ticket.py
+++ flight_ticket.py
@@ -4,16 +4,32 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+import pandas as pd
+import datetime
 import time
-
-# 브라우저 꺼짐 방지 옵션
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+import os
 
 """
     1. URL ==> 페이지 번호
     2. 조회할 항목 => SELECTOR
     3. 저장방식 (CSV)
+
+"""
+
+# 브라우저 꺼짐 방지 옵션
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+service=Service(executalbe_path=ChromeDriverManager().install())
+browser=webdriver.Chrome(service=service, options=chrome_options)
+
+# 네이버 항공권으로 이동
+browser.get('https://flight.naver.com')
+
+
+
+"""
+    자동 검색
 
 """
 
@@ -39,12 +55,6 @@ def select_day(month, day):
                         return
 
 
-service=Service(executalbe_path=ChromeDriverManager().install())
-browser=webdriver.Chrome(service=service, options=chrome_options)
-
-# 네이버 항공권으로 이동
-browser.get('https://flight.naver.com')
-
 
 start_area=input("어디서 갈거야:")
 end_area=input("어디로 갈건데:")
@@ -63,8 +73,6 @@ end_list = []
 end_list = end_date.split("/")
 end_month = end_list[0] + "." + end_list[1] + "."
 end_day = end_list[2]
-
-
 
 
 # 버튼 찾기
@@ -123,3 +131,59 @@ search = browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[4]/div/di
 search.click()
 browser.implicitly_wait(10)
 
+
+"""
+항공권 크롤링
+
+"""
+# 항공권 구간
+all_flight_tickets=[]
+
+# 데이터 담을 변수
+tickets=[]
+
+# 항공권 개수
+cnt=100
+
+
+# 항공권 모든 데이터 크롤링하기
+# all_flight_tickets = browser.find_elements(By.CSS_SELECTOR,'#container > div.international_content__2Z9HD > div > div.concurrent_ConcurrentList__1EKaB > div > div > div.concurrent_schedule__1Na5l > div')
+
+# 항공권 구간 나누기 
+all_flight_tickets=browser.find_elements(By.CLASS_NAME,'concurrent_ConcurrentItemContainer__2lQVG')
+
+
+# 항공권 크롤링하기
+for l, val in enumerate(all_flight_tickets):
+    if l < cnt:
+        try:
+            # 각 항공권 요소마다 시간 가져오기
+            time = val.find_elements(By.CLASS_NAME,'route_time__-2Z1T')
+            tickets.append({
+                # '로고': val.find_element(By.XPATH,'//*[@id="container"]/div/div/div/div/div/div/div/div/div/span/img')
+                '항공': val.find_element(By.CLASS_NAME,'airline_name__Tm2wJ').text,
+                'CO2' : val.find_element(By.CLASS_NAME,'emissions_value__AlxrP').text,
+                '출발 시간' : time[0].text,
+                '도착 시간' : time[1].text,
+                '소요 시간' : val.find_elements(By.CLASS_NAME,'route_info__1RhUH')[0].text,
+                '출발 시간' : time[2].text,
+                '도착 시간' : time[3].text,
+                '소요 시간' : val.find_elements(By.CLASS_NAME,'route_info__1RhUH')[1].text,
+                '카드사' : val.find_element(By.CLASS_NAME,'item_type__2KJOZ').text,
+                '가격' : val.find_element(By.CLASS_NAME,'item_usual__dZqAN').text
+
+            })
+        except:
+            continue
+
+
+# list -> dataframe
+flight_ticket_df=pd.DataFrame(tickets)
+
+### 저장 디렉토리 생성
+os.makedirs('tickets_info', exist_ok=True)
+### 파일명-%Y-%m-%d
+c_day=datetime.date.today().strftime("%Y-%m-%d")
+file_path=f"tickets_info/{c_day}.csv"
+flight_ticket_df.to_csv(file_path, index=False)
+print("============완료=============")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,9 @@
+flight_ticket_df=pd.DataFrame(tickets)
 
-# browser.close()
+# ### 저장 디렉토리 생성
+# os.makedirs('tickets_info', exist_ok=True)
+# ### 파일명-%Y-%m-%d
+# c_day=datetime.date.today().strftime("%Y-%m-%d")
+# file_path=f"tickets_info/{c_day}.csv"
+# flight_ticket_df.to_csv(file_path, index=False)
+# print("============완료=============")
